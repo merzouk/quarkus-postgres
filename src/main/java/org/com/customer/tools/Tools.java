552e58e0159661c77520a98849de7008f84e013f
Merzouk MENHOUR <merzouk@gmail.com>
2023-06-17 17:21:15 +0200

    modifs

--- src/main/java/org/com/customer/tools/Tools.java
+++ src/main/java/org/com/customer/tools/Tools.java
@@ -3,7 +3,7 @@ package org.com.customer.tools;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Tools {
+public final class Tools {
 
     private static final String regex = "^[a-zA-Z0-9_!#$%&amp;'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 
