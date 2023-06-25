f63ff8dcbb1ed6ed58e975e196eac91b3b004187
Merzouk MENHOUR <merzouk@gmail.com>
2023-06-25 11:08:00 +0200

    modifs

--- src/main/java/org/com/customer/services/CustomerService.java
+++ src/main/java/org/com/customer/services/CustomerService.java
@@ -1,4 +1,3 @@
-
 package org.com.customer.services;
 
 import java.util.*;
@@ -25,30 +24,31 @@ import lombok.extern.slf4j.Slf4j;
 /**
  *
  * A Renseigner.
- * @author  : admin
+ *
+ * @author : admin
  * @project : customer-api-0.0.1
  * @package : org.com.customer.services
- * @date    : 21 mai 2023 13:29:01
+ * @date : 21 mai 2023 13:29:01
  */
 @ApplicationScoped
 @AllArgsConstructor
 @Slf4j
-public class CustomerService
-{
-    @Inject
-    CustomerRepository  customerRepository;
+public class CustomerService {
 
     @Inject
-    CustomerMapper      customerMapper;
+    CustomerRepository customerRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger( CustomerService.class );
+    @Inject
+    CustomerMapper customerMapper;
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+
     /**
      *
      * @return
      */
-    public List<Customer> findAll()
-    {
-        return this.customerMapper.toListModel( customerRepository.findAll().list() );
+    public List<Customer> findAll() {
+        return this.customerMapper.toListModel(customerRepository.findAll().list());
     }
 
     /**
@@ -56,14 +56,13 @@ public class CustomerService
      * @param customerId
      * @return
      */
-    public Optional<Customer> findById( @NonNull Integer customerId )
+    public Optional<Customer> findById(@NonNull Integer customerId)
     {
-        if( Objects.isNull( customerId ) || customerId <= 0)
-        {
-            logger.error("Find Customer by customerId not valid : "+customerId);
-            throw new CustomerException( "Find Customer by customerId not valid : "+customerId );
+        if (Objects.isNull(customerId) || customerId <= 0) {
+            logger.error("Find Customer by customerId not valid : " + customerId);
+            throw new CustomerException("Find Customer by customerId not valid : " + customerId);
         }
-        return customerRepository.findByIdOptional( customerId ).map( customerMapper::toModel);
+        return customerRepository.findByIdOptional(customerId).map(customerMapper::toModel);
     }
 
     /**
@@ -72,14 +71,14 @@ public class CustomerService
      * @return
      */
     @Transactional(rollbackOn = Exception.class)
-    public boolean deleteById( @NonNull Integer customerId )
+    public boolean deleteById(@NonNull Integer customerId)
     {
-        if( Objects.isNull( customerId ) || customerId <= 0)
+        if (Objects.isNull(customerId) || customerId <= 0)
         {
-            logger.error("Delete Customer by customerId not valid : "+customerId);
-            throw new CustomerException( "Delete Customer by customerId not valid : "+customerId );
+            logger.error("Delete Customer by customerId not valid : " + customerId);
+            throw new CustomerException("Delete Customer by customerId not valid : " + customerId);
         }
-        return customerRepository.deleteById( customerId );
+        return customerRepository.deleteById(customerId);
     }
 
     /**
@@ -87,14 +86,14 @@ public class CustomerService
      * @param firstname
      * @return
      */
-    public List<Customer> findByFirstName( @NonNull String firstname )
+    public List<Customer> findByFirstName(@NonNull String firstname)
     {
-        if( Objects.isNull( firstname ) || firstname.length() == 0 )
+        if (Objects.isNull(firstname) || firstname.length() == 0)
         {
-            logger.error("Find Customer List by firstname not valid : "+firstname);
-            throw new CustomerException( "Find Customer List by firstname not valid : "+firstname );
+            logger.error("Find Customer List by firstname not valid : " + firstname);
+            throw new CustomerException("Find Customer List by firstname not valid : " + firstname);
         }
-        return this.customerMapper.toListModel( customerRepository.list( "first_name", firstname ) );
+        return this.customerMapper.toListModel(customerRepository.list("first_name", firstname));
     }
 
     /**
@@ -103,18 +102,18 @@ public class CustomerService
      * @param lastname
      * @return
      */
-    public List<Customer> findByFirstNameAndLastName( @NonNull String firstname , @NonNull String lastname)
+    public List<Customer> findByFirstNameAndLastName(@NonNull String firstname, @NonNull String lastname)
     {
-        if( Objects.isNull( firstname ) || Objects.isNull( lastname ) || firstname.length() == 0 || lastname.length() == 0)
+        if (Objects.isNull(firstname) || Objects.isNull(lastname) || firstname.length() == 0 || lastname.length() == 0)
         {
-            logger.error("Find Customer List by firstname: "+firstname +"  And lastname: "+lastname+" not valid");
-            throw new CustomerException( "Find Customer List by firstname: "+firstname +"  And lastname: "+lastname+" not valid");
+            logger.error("Find Customer List by firstname: " + firstname + "  And lastname: " + lastname + " not valid");
+            throw new CustomerException("Find Customer List by firstname: " + firstname + "  And lastname: " + lastname + " not valid");
         }
         String query = "FROM Customer c WHERE c.firstName =: first_name and c.lastName =: last_name";
         Map<String, Object> params = new HashMap<String, Object>();
-        params.put( "first_name", firstname );
-        params.put( "last_name",  lastname );
-        return this.customerMapper.toListModel( customerRepository.list( query, params ) );
+        params.put("first_name", firstname);
+        params.put("last_name", lastname);
+        return this.customerMapper.toListModel(customerRepository.list(query, params));
     }
 
     /**
@@ -122,14 +121,14 @@ public class CustomerService
      * @param lastname
      * @return
      */
-    public List<Customer> findByLastName( @NonNull String lastname )
+    public List<Customer> findByLastName(@NonNull String lastname)
     {
-        if( Objects.isNull( lastname ) || lastname.length() == 0 )
+        if (Objects.isNull(lastname) || lastname.length() == 0)
         {
-            logger.error("Find Customer List by lastname not valid : "+lastname);
-            throw new CustomerException( "Find Customer List by lastname not valid : "+lastname );
+            logger.error("Find Customer List by lastname not valid : " + lastname);
+            throw new CustomerException("Find Customer List by lastname not valid : " + lastname);
         }
-        return this.customerMapper.toListModel( customerRepository.list( "last_name", lastname ) );
+        return this.customerMapper.toListModel(customerRepository.list("last_name", lastname));
     }
 
     /**
@@ -137,91 +136,91 @@ public class CustomerService
      * @param email
      * @return
      */
-    public Optional<Customer> findByEmail( @NonNull String email )
+    public Optional<Customer> findByEmail(@NonNull String email)
     {
-        if( Objects.isNull( email ) )
+        if (Objects.isNull(email))
         {
-            logger.error("Find Customer by email not valid : "+email);
-            throw new CustomerException( "Find Customer by email not valid : "+email );
+            logger.error("Find Customer by email not valid : " + email);
+            throw new CustomerException("Find Customer by email not valid : " + email);
         }
-        if(!Tools.validateEmail(email))
+        if (!Tools.validateEmail(email))
         {
-            logger.error("email is not valid : "+email);
-            throw new CustomerException( "email is not valid : "+email );
+            logger.error("email is not valid : " + email);
+            throw new CustomerException("email is not valid : " + email);
         }
-        return customerRepository.find( "email", email ).firstResultOptional().map( customerMapper::toModel);
+        return customerRepository.find("email", email).firstResultOptional().map(customerMapper::toModel);
     }
 
     @Transactional(rollbackOn = Exception.class)
-    public void save( @Valid Customer customer )
+    public void save(@Valid Customer customer)
     {
-        if( Objects.isNull( customer ) )
+        if (Objects.isNull(customer))
         {
             logger.error("Saving new Customer not valid ");
-            throw new CustomerException( "Saving new Customer not valid" );
+            throw new CustomerException("Saving new Customer not valid");
         }
-        if(!Tools.validateEmail(customer.getEmail()))
+        if (!Tools.validateEmail(customer.getEmail()))
         {
-            logger.error("email is not valid : "+customer.getEmail());
-            throw new CustomerException( "email is not valid : "+customer.getEmail() );
+            logger.error("email is not valid : " + customer.getEmail());
+            throw new CustomerException("email is not valid : " + customer.getEmail());
         }
-        if( Objects.isNull( customer.getFirstName() ) )
+        if (Objects.isNull(customer.getFirstName()))
         {
-            logger.error("Saving new Customer not valid for firstname "+customer.getFirstName());
-            throw new CustomerException( "Saving new Customer not valid for firstname "+customer.getFirstName() );
+            logger.error("Saving new Customer not valid for firstname " + customer.getFirstName());
+            throw new CustomerException("Saving new Customer not valid for firstname " + customer.getFirstName());
         }
-        if( Objects.isNull( customer.getLastName() ) )
+        if (Objects.isNull(customer.getLastName()))
         {
-            logger.error("Saving new Customer not valid for lastname "+customer.getLastName());
-            throw new CustomerException( "Saving new Customer not valid for lastname "+customer.getLastName() );
+            logger.error("Saving new Customer not valid for lastname " + customer.getLastName());
+            throw new CustomerException("Saving new Customer not valid for lastname " + customer.getLastName());
         }
-        if( Objects.isNull( customer.getPhone() ) )
+        if (Objects.isNull(customer.getPhone()))
         {
-            logger.error("Saving new Customer not valid for Phone number "+customer.getPhone());
-            throw new CustomerException( "Saving new Customer not valid for Phone number "+customer.getPhone() );
+            logger.error("Saving new Customer not valid for Phone number " + customer.getPhone());
+            throw new CustomerException("Saving new Customer not valid for Phone number " + customer.getPhone());
         }
-        if( !Tools.isNumeric(customer.getPhone() ) )
+        if (!Tools.isNumeric(customer.getPhone()))
         {
-            logger.error("Saving new Customer not valid for Phone number \""+customer.getPhone() +"\" Phone number is not numeric value");
-            throw new CustomerException( "Saving new Customer not valid for Phone number \""+customer.getPhone() +"\" Phone number is not numeric value");
+            logger.error("Saving new Customer not valid for Phone number \"" + customer.getPhone() + "\" Phone number is not numeric value");
+            throw new CustomerException("Saving new Customer not valid for Phone number \"" + customer.getPhone() + "\" Phone number is not numeric value");
         }
-        logger.debug( "Saving Customer : {}", customer.toString() );
-        CustomerEntity entity = customerMapper.toEntity( customer );
+        logger.debug("Saving Customer : {}", customer.toString());
+        CustomerEntity entity = customerMapper.toEntity(customer);
         entity.setCustomerId(getMaxCustomerId() + 1);
-        customerRepository.persist( entity );
-        customerMapper.updateModelFromEntity( entity, customer );
+        customerRepository.persist(entity);
+        customerMapper.updateModelFromEntity(entity, customer);
     }
 
     @Transactional(rollbackOn = Exception.class)
-    public void update( @Valid Customer customer )
+    public void update(@Valid Customer customer)
     {
-        if( Objects.isNull( customer ) )
+        if (Objects.isNull(customer))
         {
             logger.error("updating Customer not valid ");
-            throw new CustomerException( "updating Customer not valid" );
+            throw new CustomerException("updating Customer not valid");
         }
-        logger.debug( "Updating Customer : {}", customer.toString() );
-        if( Objects.isNull( customer.getCustomerId() ) )
+        logger.debug("Updating Customer : {}", customer.toString());
+        if (Objects.isNull(customer.getCustomerId()))
         {
             logger.error("Customer does not have a customerId");
-            throw new CustomerException( "Customer does not have a customerId" );
+            throw new CustomerException("Customer does not have a customerId");
         }
-        if(!Tools.validateEmail(customer.getEmail()))
+        if (!Tools.validateEmail(customer.getEmail()))
         {
-            logger.error("email is not valid : "+customer.getEmail());
-            throw new CustomerException( "email is not valid : "+customer.getEmail() );
+            logger.error("email is not valid : " + customer.getEmail());
+            throw new CustomerException("email is not valid : " + customer.getEmail());
         }
-        CustomerEntity entity = customerRepository.findByIdOptional( customer.getCustomerId() )
-                .orElseThrow( () -> new CustomerException( "No Customer found for customerId[%s]", customer.getCustomerId() ) );
-        customerMapper.updateEntityFromModel( customer, entity );
-        customerRepository.persist( entity );
-        customerMapper.updateModelFromEntity( entity, customer );
+        CustomerEntity entity = customerRepository.findByIdOptional(customer.getCustomerId())
+                .orElseThrow(() -> new CustomerException("No Customer found for customerId[%s]", customer.getCustomerId()));
+        customerMapper.updateEntityFromModel(customer, entity);
+        customerRepository.persist(entity);
+        customerMapper.updateModelFromEntity(entity, customer);
     }
 
     private Integer getMaxCustomerId()
     {
         return Panache.getEntityManager()
-                .createQuery( "SELECT max(c.customerId) FROM Customer c", Integer.class )
+                .createQuery("SELECT max(c.customerId) FROM Customer c", Integer.class)
                 .getSingleResult();
     }
 }
